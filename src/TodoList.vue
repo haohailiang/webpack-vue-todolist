bc4ef4518ede5606fd86efb264159d861dc759b4
郝海亮 <2298763729@qq.com>
2018-05-29 12:04:49 +0800

    Initial commit

---
+++ src/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<input type="text" v-model="todoInputValue" @keyup.enter="handleSubmit">
+		<todo-item
+			v-for="(item, index) of todoLists"
+			:item=item
+			:index=index
+			:key=item
+			@delitem="handleDelClick">
+		</todo-item>
+	</div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+
+export default {
+	data() {
+		return {
+			todoInputValue    : '',
+			todoLists         : ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+		}
+	},
+	components: {
+		'todo-item' : TodoItem
+	},
+	methods: {
+		handleSubmit(){
+			this.todoLists.push(this.todoInputValue);
+			this.todoInputValue = '';
+		},
+		handleDelClick(index){
+			this.todoLists.splice(index, 1);
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<div @click="handleDelClick">
+			值 : {{ item }}&nbsp;&nbsp;&nbsp;序号 : {{ index }}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props    : ['item', 'index'],
+	data () {
+		return {
+			msg: 'Welcome to Your Vue.js App'
+		}
+	},
+	methods  : {
+		handleDelClick(){
+			this.$emit('delitem', this.index)
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
